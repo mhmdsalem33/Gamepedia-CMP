fd697bf3cf0ef20def044559382ad32a0d6111a6
mhmdsalem33 <mhmd.salem33@yahoo.com>
2025-08-14 17:05:34 +0300

    fix a scroll to left in platforms

--- game/ui/src/commonMain/kotlin/org/gamepdia/ui/gameDetails/GameDetailsScreen.kt
+++ game/ui/src/commonMain/kotlin/org/gamepdia/ui/gameDetails/GameDetailsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -119,8 +120,10 @@ fun GameDetailsScreenContent(
                         style = MaterialTheme.typography.bodyLarge
                     )
 
-                    LazyRow(modifier = Modifier
-                        .padding(start = 10.dp , bottom = 30.dp)
+                    LazyRow(
+                        contentPadding = PaddingValues(start = 10.dp),
+                        modifier = Modifier
+                        .padding(bottom = 30.dp)
                         .fillMaxWidth()){
 
                         items(data.platforms ?: emptyList() ){ platform ->
